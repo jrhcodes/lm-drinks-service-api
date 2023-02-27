29497f36d3a20e7e1ac0c0e82b45b4ddf45b4bb0
Julian <mrjrhirst@gmail.com>
2023-02-27 18:34:39 +0000

    feat: Implemented 'Part 7 - Latte Coffee test

--- src/controllers/coffe_request.test.ts
+++ src/controllers/coffe_request.test.ts
@@ -1,11 +1,12 @@
 import request from 'supertest';
 import { app } from '../app';
-
-describe('Test home API endpoint request', () => {
-    test('GET should return correct message', async () => {
-        const res = await request(app).get('/coffee');
-        expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('I like coffee!');
-        //expect(res.text).toEqual('This should fail!');
+test('GET /coffee should return correct object', async () => {
+    const res = await request(app)
+        .get('/coffee')
+        .query({ coffeeName: 'Latte' });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+        drinkType: 'Coffee',
+        name: 'Latte',
     });
 });
